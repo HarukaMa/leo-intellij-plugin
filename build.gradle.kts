6eec337dcd5b3d57f4ad0b72612e91d93d802e4b
Haruka <mrx@hcc.im>
2022-08-24 18:07:10 +0900

    add find usage impl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2022 Haruka Ma
+ * This file is part of Leo / Aleo IntelliJ plugin.
+ *
+ * Leo / Aleo IntelliJ plugin is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Leo / Aleo IntelliJ plugin is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Leo / Aleo IntelliJ plugin. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.6.20"
@@ -5,7 +21,7 @@ plugins {
 }
 
 group = "im.mrx"
-version = "0.0.1"
+version = "0.0.1-1"
 
 sourceSets["main"].java.srcDirs("src/main/gen")
 
@@ -45,5 +61,6 @@ tasks {
 
     publishPlugin {
         token.set(System.getenv("PUBLISH_TOKEN"))
+        channels.set(listOf("Preview"))
     }
 }
---
+++ src/main/kotlin/im/mrx/leolanguage/leo/LeoFindUsagesProvider.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2022 Haruka Ma
+ * This file is part of Leo / Aleo IntelliJ plugin.
+ *
+ * Leo / Aleo IntelliJ plugin is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Leo / Aleo IntelliJ plugin is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Leo / Aleo IntelliJ plugin. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package im.mrx.leolanguage.leo
+
+import com.intellij.lang.cacheBuilder.DefaultWordsScanner
+import com.intellij.lang.cacheBuilder.WordsScanner
+import com.intellij.lang.findUsages.FindUsagesProvider
+import com.intellij.psi.PsiElement
+import com.intellij.psi.tree.TokenSet
+import im.mrx.leolanguage.leo.psi.LeoNamedElement
+import im.mrx.leolanguage.leo.psi.LeoTypes.*
+
+class LeoFindUsagesProvider : FindUsagesProvider {
+
+    override fun getWordsScanner(): WordsScanner {
+        return DefaultWordsScanner(
+            LeoLexerAdapter(),
+            TokenSet.create(IDENTIFIER),
+            TokenSet.create(BLOCK_COMMENT, END_OF_LINE_COMMENT),
+            TokenSet.create(
+                ADDRESS_LITERAL, BOOLEAN_LITERAL, STRING_LITERAL, NUMERIC_LITERAL
+            )
+        )
+    }
+
+    override fun canFindUsagesFor(psiElement: PsiElement): Boolean {
+        return psiElement is LeoNamedElement
+    }
+
+    override fun getHelpId(psiElement: PsiElement): String? {
+        return null
+    }
+
+    override fun getType(element: PsiElement): String {
+        return "TODO Type"
+    }
+
+    override fun getDescriptiveName(element: PsiElement): String {
+        return (element as? LeoNamedElement)?.name ?: ""
+    }
+
+    override fun getNodeText(element: PsiElement, useFullName: Boolean): String {
+        return "TODO NodeText"
+    }
+}
