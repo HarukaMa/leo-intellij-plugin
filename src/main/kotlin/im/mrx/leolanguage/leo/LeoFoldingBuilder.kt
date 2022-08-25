69c580b7b9a4c484950f64493b3faf5de1dc9fa5
Haruka <mrx@hcc.im>
2022-08-25 10:55:34 +0900

    test code folding

---
+++ src/main/kotlin/im/mrx/leolanguage/leo/LeoFoldingBuilder.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2022 Haruka Ma
+ * This file is part of Leo / Aleo IntelliJ plugin.
+ *
+ * Leo / Aleo IntelliJ plugin is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Leo / Aleo IntelliJ plugin is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Leo / Aleo IntelliJ plugin. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package im.mrx.leolanguage.leo
+
+import com.intellij.lang.ASTNode
+import com.intellij.lang.folding.FoldingBuilderEx
+import com.intellij.lang.folding.FoldingDescriptor
+import com.intellij.openapi.editor.Document
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import im.mrx.leolanguage.leo.psi.LeoRecordDeclaration
+import im.mrx.leolanguage.leo.psi.LeoVisitor
+
+class LeoFoldingBuilder : FoldingBuilderEx() {
+    override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
+        val descriptors = ArrayList<FoldingDescriptor>()
+        val visitor = FoldingVisitor(descriptors)
+        PsiTreeUtil.processElements(root) {
+            it.accept(visitor)
+            true
+        }
+        return descriptors.toTypedArray()
+    }
+
+    private class FoldingVisitor(val descriptors: ArrayList<FoldingDescriptor>) : LeoVisitor() {
+        override fun visitRecordDeclaration(o: LeoRecordDeclaration) {
+            val block = o.circuitComponentDeclarations ?: return
+            descriptors.add(FoldingDescriptor(block, block.textRange))
+        }
+    }
+
+    override fun getPlaceholderText(node: ASTNode): String {
+        return "{ ... }"
+    }
+
+    override fun isCollapsedByDefault(node: ASTNode): Boolean {
+        return false
+    }
+
+}
